a26f47b4de26014a3745ab141622875098b8a356
Rob Spoor <rob@icemanx.nl>
2020-01-08 21:34:01 +0100

    Replaced boolean for case sensitivity with an enum. Disallowed duplicate keys per case sensitivity in YAMLObfuscator.builder

--- src/main/java/com/github/robtimus/obfuscation/yaml/YAMLObfuscator.java
+++ src/main/java/com/github/robtimus/obfuscation/yaml/YAMLObfuscator.java
@@ -17,6 +17,7 @@
 
 package com.github.robtimus.obfuscation.yaml;
 
+import static com.github.robtimus.obfuscation.CaseSensitivity.CASE_SENSITIVE;
 import static com.github.robtimus.obfuscation.ObfuscatorUtils.checkStartAndEnd;
 import static com.github.robtimus.obfuscation.ObfuscatorUtils.copyTo;
 import static com.github.robtimus.obfuscation.ObfuscatorUtils.discardAll;
@@ -45,6 +46,7 @@ import org.snakeyaml.engine.v2.parser.Parser;
 import org.snakeyaml.engine.v2.parser.ParserImpl;
 import org.snakeyaml.engine.v2.scanner.StreamReader;
 import com.github.robtimus.obfuscation.CachingObfuscatingWriter;
+import com.github.robtimus.obfuscation.CaseSensitivity;
 import com.github.robtimus.obfuscation.Obfuscator;
 import com.github.robtimus.obfuscation.ObfuscatorUtils.MapBuilder;
 
@@ -401,15 +403,16 @@ public final class YAMLObfuscator extends Obfuscator {
 
         /**
          * Adds a property to obfuscate.
-         * This method is an alias for {@link #withProperty(String, Obfuscator, boolean) withProperty(property, obfuscator, true)}.
+         * This method is an alias for {@link #withProperty(String, Obfuscator, CaseSensitivity) withProperty(property, obfuscator, CASE_SENSITIVE)}.
          *
          * @param property The name of the property. It will be treated case sensitively.
          * @param obfuscator The obfuscator to use for obfuscating the property.
          * @return This object.
          * @throws NullPointerException If the given property name or obfuscator is {@code null}.
+         * @throws IllegalArgumentException If a property with the same name and the same case sensitivity was already added.
          */
         public Builder withProperty(String property, Obfuscator obfuscator) {
-            return withProperty(property, obfuscator, true);
+            return withProperty(property, obfuscator, CASE_SENSITIVE);
         }
 
         /**
@@ -417,13 +420,13 @@ public final class YAMLObfuscator extends Obfuscator {
          *
          * @param property The name of the property.
          * @param obfuscator The obfuscator to use for obfuscating the property.
-         * @param caseSensitive {@code true} if the property name should be treated case sensitively,
-         *                          or {@code false} if it should be treated case insensitively.
+         * @param caseSensitivity The case sensitivity for the key.
          * @return This object.
-         * @throws NullPointerException If the given property name or obfuscator is {@code null}.
+         * @throws NullPointerException If the given property name, obfuscator or case sensitivity is {@code null}.
+         * @throws IllegalArgumentException If a property with the same name and the same case sensitivity was already added.
          */
-        public Builder withProperty(String property, Obfuscator obfuscator, boolean caseSensitive) {
-            obfuscators.withEntry(property, obfuscator, caseSensitive);
+        public Builder withProperty(String property, Obfuscator obfuscator, CaseSensitivity caseSensitivity) {
+            obfuscators.withEntry(property, obfuscator, caseSensitivity);
             return this;
         }
 
