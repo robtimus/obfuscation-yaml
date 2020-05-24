f7b0985e3b1a5883d9e43a09b03ec2a0af2d75b7
Rob Spoor <rob@icemanx.nl>
2020-05-24 17:42:41 +0200

    Replaced automatic module name with a module descriptor

---
+++ src/moditect/module-info.java
@@ -0,0 +1,7 @@
+module com.github.robtimus.obfuscation.yaml {
+    requires transitive com.github.robtimus.obfuscation;
+    requires org.snakeyaml.engine.v2;
+    requires org.slf4j;
+
+    exports com.github.robtimus.obfuscation.yaml;
+}
