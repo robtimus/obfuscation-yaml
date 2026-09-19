99a21c1a8421cdb50691cbdc383faa630ed06e29
Rob Spoor <rob@icemanx.nl>
2026-09-19 14:13:40 +0200

    Upgraded snakeyaml version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,7 +20,7 @@
  */
 module com.github.robtimus.obfuscation.yaml {
     requires transitive com.github.robtimus.obfuscation;
-    requires org.snakeyaml.engine.v2;
+    requires org.snakeyaml.engine;
     requires org.slf4j;
 
     exports com.github.robtimus.obfuscation.yaml;
